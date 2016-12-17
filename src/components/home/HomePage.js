2ae3bf51224ad81cd8648fe2d4c2c6d803fb40e6
Stephen Ward <stephen@scalablegroup.com>
2016-12-16 20:21:22 -0800

    added containers

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,12 +3,24 @@ import HomeLogoContainer from './components/HomeLogoContainer'
 import HomeWorkContainer from './components/HomeWorkContainer'
 import HomeAboutContainer from './components/HomeAboutContainer'
 import HomeContactContainer from './components/HomeContactContainer'
+import Waypoint from 'react-waypoint'
 
-const HomePage = () =>
+class HomePage extends React.Component {
+
+  _handleWaypointEnter() {
+  }
+
+  render() {
+    return(
   <div id="homeContainer">
     <HomeLogoContainer />
     <HomeWorkContainer />
+      <Waypoint onEnter={this._handleWaypointEnter} />
     <HomeAboutContainer />
     <HomeContactContainer />
   </div>
+);
+}
+}
+
 export default HomePage
--- src/components/home/components/HomeWorkContainer.js
+++ src/components/home/components/HomeWorkContainer.js
@@ -1,12 +1,21 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 
 const HomeWorkContainer = () =>
   <div id="homeWorkContainer">
     <div className="homeWorkContent">
-      <h1>Our Work</h1>
-      <Link className="btn btn-primary btn-lg" to="/our-work" activeClassName="active">See More</Link>
+      <div className="row">
+        <div className="container">
+        <div className="col-md-6">test</div>
+        <div className="col-md-6">
+          <p>
+            Exceptional Integrations is a full-service residential and commercial custom electronics and design integration firm. Our primary goal is to make your home or business systems, safe, elegant, fun and easy to use. Our team is comprised of world-class experts in every field whose number one priority is to provide the best products and services to our clients no matter how big or small the job is.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/our-work" activeClassName="active">See More</Link>
+        </div>
+        </div>
     </div>
+  </div>
   </div>
 
 export default HomeWorkContainer
