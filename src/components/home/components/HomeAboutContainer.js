7c7ac990c131d2510fe0cfdc904f67938eb704b5
Stephen Ward <stephen@scalablegroup.com>
2017-01-02 14:47:00 -0800

    changed footer color, enhanced homepage promises section

--- src/components/home/components/HomeAboutContainer.js
+++ src/components/home/components/HomeAboutContainer.js
@@ -3,16 +3,40 @@ import React from 'react'
 const HomeAboutContainer = () =>
   <div id="homeAboutContainer">
     <div className="homeAboutContent">
+      <div className="container">
       <div className="row">
-        <div className="container">
         <div className="col-md-12 teamBlurb">
         <h2 className="text-center">Our Promise</h2>
-          <p>
-            Our team of seasoned technicians and reliable contractors are focused on turning upscale dreams into reality. From custom interface specialists to full-room automation experts, meet the A-team ready to deliver the setup you request.
+          <p className="text-center">
+            At the core of Exceptional Integrations is a set of values incorporated into every project we take on. From thorough installation plans to detailed walkthroughs and maintenance contracts, we put the customer first.
           </p>
         </div>
-        <div className="col-md-6">
-        </div>
+      </div>
+      <div className="row">
+      <div className="col-md-3 text-center">
+        <div className="promiseOne text-center"><i className="glyphicon glyphicon-fire"></i></div>
+        <div className="clearfix"></div>
+        <h4 className="promiseText text-center">Timeliness</h4>
+        <p className="promiseText">Praesent vestibulum cursus metus, eu maximus quam. Fusce vehicula a ligula vitae luctus. Suspendisse finibus nulla ut nisl suscipit, nec pellentesque ex dignissim. </p>
+      </div>
+      <div className="col-md-3 center">
+        <div className="promiseTwo"><i className="glyphicon glyphicon-hdd"></i></div>
+          <div className="clearfix"></div>
+          <h4 className="promiseText text-center">Follow-Through</h4>
+          <p className="promiseText">Praesent vestibulum cursus metus, eu maximus quam. Fusce vehicula a ligula vitae luctus. Suspendisse finibus nulla ut nisl suscipit, nec pellentesque ex dignissim. </p>
+      </div>
+      <div className="col-md-3 center">
+        <div className="promiseThree"><i className="glyphicon glyphicon-briefcase"></i></div>
+          <div className="clearfix"></div>
+          <h4 className="promiseText text-center">Ease-of-use</h4>
+          <p className="promiseText">Praesent vestibulum cursus metus, eu maximus quam. Fusce vehicula a ligula vitae luctus. Suspendisse finibus nulla ut nisl suscipit, nec pellentesque ex dignissim. </p>
+      </div>
+      <div className="col-md-3 center">
+        <div className="promiseFour"><i className="glyphicon glyphicon-floppy-disk"></i></div>
+          <div className="clearfix"></div>
+          <h4 className="promiseText text-center">Maintainable</h4>
+          <p className="promiseText">Praesent vestibulum cursus metus, eu maximus quam. Fusce vehicula a ligula vitae luctus. Suspendisse finibus nulla ut nisl suscipit, nec pellentesque ex dignissim. </p>
+      </div>
       </div>
     </div>
     </div>
