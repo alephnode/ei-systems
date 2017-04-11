83c8190d80dde6e441dec46c83a1cb3d8e85a113
Stephen Ward <srmward@gmail.com>
2017-04-10 18:23:58 -0700

    updating video

--- src/components/home/components/HomeLogoContainer.js
+++ src/components/home/components/HomeLogoContainer.js
@@ -28,7 +28,7 @@ class HomeLogoContainer extends React.Component {
   return (
   <div id="homeLogoContainer">
     <video playsInline autoPlay muted loop poster="" id="bgvid">
-    <source src={require('../../../movies/ei_systems.mp4')} type="video/mp4" />
+    <source src={require('../../../movies/ei_systems_new.mp4')} type="video/mp4" />
     </video>
     <img role="presentation" className="ei_logo img-responsive" src={require('../../../images/flat_ei.png')}/>
     <div className="scrollPrompt" style={{animation: this.state.animation}}>SCROLL TO CONTINUE<br/><i className="glyphicon glyphicon-chevron-down bounce"></i></div>
