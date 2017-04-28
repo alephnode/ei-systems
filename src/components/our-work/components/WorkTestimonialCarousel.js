8fdddf8e47e5a854a0246a080a0f5d473b38df83
Stephen Ward <srmward@gmail.com>
2017-04-28 11:14:37 -0700

    fixed slider on work page

--- src/components/our-work/components/WorkTestimonialCarousel.js
+++ src/components/our-work/components/WorkTestimonialCarousel.js
@@ -1,58 +1,58 @@
 import React from 'react'
-import Slider from 'react-slick'
+import testimonials from '../../../testimonials/testimonials'
 
-const settings = {
-  autoplay: true,
-  autoplaySpeed: 9000,
-  infinite: true,
-  fade: true
-};
+class WorkTestimonialCarousel extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      author: '',
+      count: 0,
+      slideFade: ''
+    }
+    this._updateSlider = this._updateSlider.bind(this)
+  }
 
-const WorkTestimonialCarousel = () =>
-  <div id="workTestimonialCarouselContainer">
-    <Slider {...settings}>
-      <div className="workTestimonialPhoto" id="carouselOne">
-        <div className="testimonialCarouselCard">
-          <p className="testimonial">“My complements to the entire team at Exceptional Integrations. It’s
-      evident that their work is not only their everyday activity but their passion no matter
-      how big or small the task is. I will use their services for the rest of my life&quot;</p><br/><div className="attribution text-right">- John Stevens Sr.</div>
-        </div>
-      </div>
-      <div className="workTestimonialPhoto" id="carouselTwo">
-        <div className="testimonialCarouselCard">
-          <p className="testimonial">“Exceptional Integrations has proven that they not only provide the
-      very best products, but they offer the very best service as well. We look forward to
-      working with you again and again.&quot;</p> <br/><div className="attribution text-right">- Joseph Szabo - Direct Buy Service Manager</div>
-        </div>
-      </div>
-      <div className="workTestimonialPhoto" id="carouselThree">
-        <div className="testimonialCarouselCard">
-          <p className="testimonial">“Exceptional Integrations biggest value is after the installation. They are responsive and ensure complete satisfaction months after the sale. They stand
-      behind what they install.&quot;</p><br/><div className="attribution text-right">- Nabil Sakab - Procter &amp; Gamble R&amp;D Senior Vice President</div>
-        </div>
-      </div>
-      <div className="workTestimonialPhoto" id="carouselFour">
-        <div className="testimonialCarouselCard">
-          <p className="testimonial">“It’s rare to see a company that will go above and beyond their call
-      of duty to ensure complete customer satisfaction as Exceptional Integrations. Thank you
-      for taking care of me whether is 2 in the afternoon or 2 in the morning!!!&quot;</p><br/><div className="attribution text-right">- Francisco Armendariz</div>
-        </div>
-      </div>
-      <div className="workTestimonialPhoto" id="carouselFive">
-        <div className="testimonialCarouselCard">
-          <p className="testimonial">“Anybody can hook up electronics, but knowing how, why and make
-      all work together so easily takes a professional from Exceptional Integrations. From start
-      to finish they know how to get the job done&quot;</p><br/><div className="attribution text-right">- Richard Stavanotsky</div>
-        </div>
-      </div>
-      <div className="workTestimonialPhoto" id="carouselSix">
-        <div className="testimonialCarouselCard">
-          <p className="testimonial">“Exceptional Integrations offers personal ongoing and timely
-      service, something most companies do not. I highly recommend Exceptional
-      Integrations for Crestron installation and follow-up service.”</p><br/><div className="attribution text-right">– K.R. at Enclave Court</div>
-        </div>
-      </div>
-    </Slider>
-  </div>
+  componentDidMount() {
+    this._updateSlider()
+    this.interval = setInterval(this._updateSlider, 7000);
+  }
 
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  _updateSlider() {
+
+      if(this.state.count === testimonials.length - 1)
+        this.state.count = 0
+
+        if(this.state.count % 2 === 0) {
+          this.setState({
+            text: testimonials[this.state.count].text,
+            author: testimonials[this.state.count].author,
+            count: this.state.count + 1
+          })
+        }
+        else {
+          this.setState({
+            text: testimonials[this.state.count].text,
+            author: testimonials[this.state.count].author,
+            count: this.state.count + 1
+          })
+        }
+  }
+
+  render() {
+    return(
+      <div id="workTestimonialCarouselContainer">
+          <div className="workTestimonialPhoto">
+            <div className="testimonialCarouselCard" style={{animation: this.state.slideFade}}>
+              <p className="testimonial">{this.state.text}</p><br/><div className="attribution text-right"> – {this.state.author}</div>
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
 export default WorkTestimonialCarousel
---
+++ src/testimonials/testimonials.js
@@ -0,0 +1,28 @@
+let testimonials = [
+  {
+    author: 'John Stevens Sr.',
+    text: 'My complements to the entire team at Exceptional Integrations. It\'s evident that their work is not only their everyday activity but their passion no matter how big or small the task is. I will use their services for the rest of my life'
+  },
+  {
+    author: 'Joseph Szabo - Direct Buy Service Manager',
+    text: ' Exceptional Integrations has proven that they not only provide the very best products, but they offer the very best service as well. We look forward to working with you again and again'
+  },
+  {
+    author: 'Nabil Sakab - Procter & Gamble R&D Senior Vice President',
+    text: 'Exceptional Integrations biggest value is after the installation. They are responsive and ensure complete satisfaction months after the sale. They stand behind what they install.'
+  },
+  {
+    author: 'Francisco Armendariz',
+    text: 'It’s rare to see a company that will go above and beyond their call of duty to ensure complete customer satisfaction as Exceptional Integrations. Thank you for taking care of me whether is 2 in the afternoon or 2 in the morning!!!'
+  },
+  {
+    author: 'Richard Stavanotsky',
+    text: 'Anybody can hook up electronics, but knowing how, why and make all work together so easily takes a professional from Exceptional Integrations. From start to finish they know how to get the job done'
+  },
+  {
+    author: 'K.R. at Enclave Court',
+    text: 'Exceptional Integrations offers personal ongoing and timely service, something most companies do not. I highly recommend Exceptional Integrations for Crestron installation and follow-up service.'
+  }
+]
+
+export default testimonials
